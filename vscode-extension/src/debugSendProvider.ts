d3e2a2b8ef2e2171b8de9f095da831fbdda01ab4
ななっち <nanachi@hotmail.co.jp>
2025-07-06 09:44:33 +0900

    aosora-analyzerを使用したCodelendsに置換

--- vscode-extension/src/debugSendProvider.ts
+++ vscode-extension/src/debugSendProvider.ts
@@ -1,56 +1,83 @@
 import * as vscode from 'vscode';
 import { SCRIPT_BLOCK_TYPE_FUNCTION, ScriptAnalyzer } from './scriptAnalyzer';
+import {Analyze, AnalyzedSourceRange, AnalyzeResult} from './scriptAnalyzer2';
 
 const functionPattern = /(function|talk)/g;
 
-export class DebugSendProvider implements vscode.CodeLensProvider {
-    
-    provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.CodeLens[]> {
-        const result:vscode.CodeLens[] = [];
+export class DebugSendProvider implements vscode.CodeLensProvider{
 
-        /*
-        const regex = new RegExp(functionPattern);
-        const text = document.getText();
-        let match;
-        while ((match = regex.exec(text)) !== null) {
-            const line = document.lineAt(document.positionAt(match.index).line);
-            const indexOf = line.text.indexOf(match[0]);
-            const position = new vscode.Position(line.lineNumber, indexOf);
-            const range = document.getWordRangeAtPosition(position, functionPattern);
-            if (range) {
-                result.push(new vscode.CodeLens(range));
-            }
-        }
-        */
+	private extensionPath:string;
+	private diag:vscode.DiagnosticCollection;
+	private lastSuccessAnalyzeResult:AnalyzeResult|null;
 
-       
+	constructor(extensionPath:string, diag: vscode.DiagnosticCollection){
+		this.extensionPath = extensionPath;
+		this.diag = diag;
+		this.lastSuccessAnalyzeResult = null;
+	}
 
-        //データを解析
-        const analyzer = new ScriptAnalyzer();
-        const blocks = analyzer.Analyze(document);
+	private MakeSourceRange(range:AnalyzedSourceRange):vscode.Range{
+		return new vscode.Range(range.line, range.column, range.endLine, range.endColumn);
+	}
+	
+	async provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): Promise<vscode.CodeLens[]> {
+		
+		//aosora-analyzerを起動
+		const analyzeResult = await Analyze(document, this.extensionPath);
+		if(!analyzeResult.error){
+			//エラーなし
+			this.diag.set(document.uri, []);
+			this.lastSuccessAnalyzeResult = analyzeResult;
+		}
+		else {
 
-        for(const item of blocks){
-            if(item.blockType == SCRIPT_BLOCK_TYPE_FUNCTION){
+			//認識できるエラーを吐いている場合、構文エラーなのでエディタに指摘内容を反映する
+			if(analyzeResult.message && analyzeResult.range){
+				const range = this.MakeSourceRange(analyzeResult.range);
+				if(range){
+				this.diag.set(document.uri, [
+						{
+							message: analyzeResult.message,
+							range: range,
+							severity: vscode.DiagnosticSeverity.Error
+						}
+					]);
+				}
+			}
+			else {
+				//認識できないエラー：仕方ないので無視
+				this.diag.set(document.uri, []);
+			}
+		}
 
-                const line = document.lineAt(document.positionAt(item.beginIndex).line);
-                const position = new vscode.Position(line.lineNumber, 0);
-                const range = document.getWordRangeAtPosition(position, functionPattern);
+		//最後に解析成功した情報を使用してCodeLendsを作る
+		//失敗時無効化していると、スクリプト記述中に表示ががたがたしてしまうため
+		if(this.lastSuccessAnalyzeResult && this.lastSuccessAnalyzeResult.functions){
+			const result:vscode.CodeLens[] = [];
+			for(const item of this.lastSuccessAnalyzeResult.functions){
+				const scriptRange = this.MakeSourceRange(item.range);
+				const script = document.getText(scriptRange);
 
-                //関数ブロックとして
-                if(range){
-                    const codeLens = new vscode.CodeLens(range);
-                    codeLens.command = {
-                        title: "<< ゴーストに送信",
-                        command: "aosora-shiori.sendToGhost",
-                        tooltip: "この関数を実行してゴーストに送信します。",
-                        arguments: [item.sendScriptBody]
-                    };
-                    result.push(codeLens);
-                }
+				//function または talk の部分があれば採用する
+				//解析エラー時、関数行のCodelends表示を維持する一方、どうみても関数ではないところに送信表示を出さないようにするため
+				const keyWordRange = document.getWordRangeAtPosition(new vscode.Position(item.range.line, item.range.column), functionPattern);;
+				if(keyWordRange){
+					const codeLens = new vscode.CodeLens(keyWordRange);
+					codeLens.command = {
+						title: "<< ゴーストに送信",
+						command: "aosora-shiori.sendToGhost",
+						tooltip: !analyzeResult.error ? "この関数を実行してゴーストに送信します。" : "スクリプトが読み込み失敗する状態のため送信できません。",
+						arguments: [script, analyzeResult.error]                        
+					};
+					result.push(codeLens);
+				}
+			}
 
-            }
-        }
+			return result;
+		}
+		else {
+			return [];
+		}
+	}
 
-        return result;
-    }
 }
