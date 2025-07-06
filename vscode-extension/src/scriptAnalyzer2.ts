f816c58b6ef1f5ecfd0bc24194ef75de234cb897
ななっち <nanachi@hotmail.co.jp>
2025-07-06 10:41:07 +0900

    あたらしいほうの解析スクリプトを追加

---
+++ vscode-extension/src/scriptAnalyzer2.ts
@@ -0,0 +1,50 @@
+import * as vscode from 'vscode';
+import { exec } from 'child_process';
+import {z} from 'zod';
+
+const AnalyzedSourceRange = z.object({
+	line: z.number(),
+	column: z.number(),
+	endLine: z.number(),
+	endColumn: z.number()
+});
+
+const AnalyzeResult = z.object({
+	error: z.boolean(),
+	message: z.optional(z.string()),
+	range: z.optional(AnalyzedSourceRange),
+	functions: z.optional(z.array(z.object({
+		range: AnalyzedSourceRange
+	})))
+});
+
+export type AnalyzedSourceRange = z.infer<typeof AnalyzedSourceRange>;
+export type AnalyzeResult = z.infer<typeof AnalyzeResult>;
+
+
+function AnalyzeScript(script:string, extensionPath:string):Promise<AnalyzeResult>{
+
+	const executablePath = extensionPath + "/" + "aosora-analyzer.exe";
+
+	return new Promise<string>((resolve, reject) => {
+		const child = exec(executablePath, (error, stdout) => {
+			if (error) {
+				reject(error);
+				return;
+			}
+			resolve(JSON.parse(stdout));
+		});
+		if (child.stdin) {
+			child.stdin.write(script);
+			child.stdin.end();
+		}
+	})
+		.then(o => AnalyzeResult.parseAsync(o))
+		.catch( () => ({error: true, message: "exec error"}));
+}
+
+//ランタイムベースのスクリプトアナライザ
+export async function Analyze(document:vscode.TextDocument, extensionPath:string){
+	const str = document.getText();
+	return await AnalyzeScript(str,extensionPath);
+}
