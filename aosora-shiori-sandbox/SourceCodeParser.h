f469e4676ffc4cfb4d29c7ab2ee73b9e9388b3d8
ななっち <nanachi@hotmail.co.jp>
2025-11-30 20:57:20 +0900

    aosora-shiori-test を aosora-shiori-sandboxに変更

---
+++ aosora-shiori-sandbox/SourceCodeParser.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <stdio.h>
+#include <string>
+#include <string_view>
+#include <list>
+#include <cassert>
+#include <regex>
+#include <vector>
+#include <map>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}													class ScriptInterpreterStack;	class ScriptFunction;	class ObjectBase;	class ScriptObject;	class ScriptVariable;	class NativeFunctionResponse;	class NativeFunctionRequest;	class ScriptExecuteContext;	class ScriptInterpreter;	//�X�N���v�g����Ăׂ�l�C�e�B�u�֐�		//���s�n	
+namespace sakura {#include "ObjectSystem.h"
